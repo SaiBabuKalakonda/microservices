e62aa8cb0604fab4921e56dc0c52c3dac9139358
SaiBabuKalakonda <saibabu0276@gmail.com>
2021-09-07 13:54:44 +0530

    added annotation...

--- src/main/java/com/htc/patientmgmt/PatientManagementApplication.java
+++ src/main/java/com/htc/patientmgmt/PatientManagementApplication.java
@@ -2,9 +2,11 @@ package com.htc.patientmgmt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableWebMvc
 public class PatientManagementApplication {
 
